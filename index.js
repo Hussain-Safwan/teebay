f098c71fe6a85eecb59494841f9fbca814affe96
Hussain Safwan <safwan.du16@gmail.com>
2024-11-02 16:23:42 +0600

    [BE] client relocated

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
-const pg = require("pg");
-const config = require("./postgre_config");
 const schema = require("./query");
-
-const client = new pg.Client(config);
-client.connect(function (err) {
-  if (err) throw err;
-  client.query("SELECT VERSION()", [], function (err, result) {
-    if (err) throw err;
-
-    console.log(result.rows[0].version);
-    client.end(function (err) {
-      if (err) throw err;
-    });
-  });
-});
 
 const app = express();
 app.use(
@@ -33,13 +18,18 @@ app.listen(PORT, () => console.log(`Server running at ${PORT}`));
 
 postgres://avnadmin:AVNS_O-W_E4kaiYwbsTwlgt8@pg-26600bda-safwan-project.j.aivencloud.com:24849/defaultdb?sslmode=require
 
-create table Player (
-    id INT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255)
+CREATE TABLE UserProfile (
+    id SERIAL PRIMARY KEY NOT NULL,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    address VARCHAR(255),
+    email VARCHAR(255),
+    phoneNumber INT,
+    password VARCHAR (255)
 );
 
-insert into Player (id, first_name, last_name) values (1, 'Hussain Md', 'Safwan');
+insert into UserProfile (firstName, lastName, address, email, phoneNumber, password) 
+values ('Hussain Md', 'Safwan', 'Dhaka, BD', 'safwan.du16@gmail.com', 761049, '123456');
 
 insert into Team (name) values ('Big_Ms');
 */
