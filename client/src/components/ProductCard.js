b6168b1691bce295ec86fa87439514f6134ea032
Hussain Safwan <safwan.du16@gmail.com>
2024-11-05 06:09:34 +0600

    [FE] confirm modal added

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ConfirmModal from "./ConfirmModal";
 
 const ProductCard = () => {
+  const [openConfirmModal, setOpenConfirmModal] = useState(false);
+
+  const handleDelete = () => {};
+
+  const closeConfirmModal = () => setOpenConfirmModal(false);
+
   return (
     <div className="product-card">
+      <ConfirmModal
+        open={openConfirmModal}
+        handleClickOpen={handleDelete}
+        handleClose={closeConfirmModal}
+      />
       <div className="first-row">
         <div>
           <h3>ASUS ZenBook</h3>
-          <div className="categories">Categories: </div>
         </div>
-        <div>Delete</div>
+        <div onClick={() => setOpenConfirmModal(true)}>
+          <DeleteIcon />
+        </div>
+      </div>
+      <div className="categories sub">Categories</div>
+      <div className="price sub">Price</div>
+
+      <div className="description">
+        You can configure the SwipeableDrawer to have a visible edge when
+        closed. If you are on a desktop, you can toggle the drawer with the
+        "OPEN" button. If you are on mobile, you can open the demo in
+        CodeSandbox ("edit" icon) and swipe.
+      </div>
+
+      <div className="footer sub">
+        <div className="date">Nov 04, 2024</div>
+        <div className="views">156</div>
       </div>
     </div>
   );
