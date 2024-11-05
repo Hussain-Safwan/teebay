084aea7ecbdfca2af3e69cf6fd0566c51ac2b75b
Hussain Safwan <safwan.du16@gmail.com>
2024-11-05 06:23:01 +0600

    [FE] landing page created

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "./ProductList";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h2>All Products</h2>
+      <ProductList />
+    </div>
+  );
+};
+
+export default LandingPage;
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import "../styles/layout.css";
 import ProductList from "./ProductList";
+import LandingPage from "./LandingPage";
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
       <div className="contents">
-        <ProductList />
+        <LandingPage />
       </div>
     </div>
   );
