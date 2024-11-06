418acd5075941a241ab84d1fd72c9408d3333d7d
Hussain Safwan <safwan.du16@gmail.com>
2024-11-06 07:33:26 +0600

    [FE] user caching

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient } from "@apollo/client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        user: {
+          read(existing = null) {
+            return existing;
+          },
+        },
+      },
+    },
+  },
+});
+
+persistCache({
+  cache,
+  storage: window.localStorage,
+});
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/api",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 export default client;
