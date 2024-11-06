30bc72605513692bda7e0e6106b197d701131aef
Hussain Safwan <safwan.du16@gmail.com>
2024-11-06 07:33:12 +0600

    [FE} navbar profile name set

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 const Navbar = () => {
+  const userQuery = gql`
+    query User {
+      user @client {
+        firstname
+      }
+    }
+  `;
+  const res = useQuery(userQuery);
+  const profileName = res?.data ? `Hi ${res.data.user.firstname}!` : "Profile";
+  console.log(res);
+
   return (
     <div className="navbar">
       <div className="logo">Teebay</div>
-      <div className="user-profile">Profile</div>
+      <div className="user-profile">{profileName}</div>
     </div>
   );
 };
